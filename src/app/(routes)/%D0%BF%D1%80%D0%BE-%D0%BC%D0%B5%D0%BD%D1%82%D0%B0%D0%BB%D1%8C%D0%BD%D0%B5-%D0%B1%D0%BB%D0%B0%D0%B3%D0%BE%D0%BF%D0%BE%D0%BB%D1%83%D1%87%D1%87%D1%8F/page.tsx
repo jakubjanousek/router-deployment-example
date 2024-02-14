2b2ccab23f3dc1c170fca5ec4379309dfb83ff8e
jakubjanousek <jakub.janousek@gmail.com>
2024-02-14 13:46:46 +0100

    Add long url detail

--- src/app/(routes)/%D0%BF%D1%80%D0%BE-%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5-%D0%B1%D0%BB%D0%B0%D0%B3%D0%BE%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D1%87%D1%8F/page.tsx
+++ src/app/(routes)/%D0%BF%D1%80%D0%BE-%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5-%D0%B1%D0%BB%D0%B0%D0%B3%D0%BE%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D1%87%D1%8F/page.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const page = () => {
-  return <div>works!</div>;
+  return (
+    <div>
+      <Link href="/%D0%BF%D1%80%D0%BE-%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5-%D0%B1%D0%BB%D0%B0%D0%B3%D0%BE%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D1%87%D1%8F/небезпечний-ґазлайтинг-не-дозволяйте-психічним-маніпуляціям-знищити-вас">
+        link to detail
+      </Link>
+      works!
+    </div>
+  );
 };
 
 export default page;
---
+++ src/app/(routes)/%D0%BF%D1%80%D0%BE-%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5-%D0%B1%D0%BB%D0%B0%D0%B3%D0%BE%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D1%87%D1%8F/небезпечний-ґазлайтинг-не-дозволяйте-психічним-маніпуляціям-знищити-вас/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>works!</div>;
+};
+
+export default page;
